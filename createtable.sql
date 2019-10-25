5f51e44d989f9d4f7be35cd9e486b0138d84ec84
Ryan <beckerr2@carleton.edu>
2019-10-25 17:18:05 -0500

    adding createtable

---
+++ createtable.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS kickstarter;
+CREATE TABLE kickstarter (
+  backers_count real,
+  blurb text,
+  category real,
+  created_at real,
+  currency text,
+  deadline real,
+  goal real,
+  launched_at real,
+  ks_name text,
+  pledged real,
+  spotlight text,
+  staff_pick text,
+  current_state text,
+  state_change real,
+  urls text
+);
