5bc505ebb01c9695e81d2ec35fd16544a66b380d
Lab User <labuser@libr451-04r.its.carleton.edu>
2019-10-31 16:16:04 -0500

    testing

--- testDatasource.py
+++ testDatasource.py
@@ -7,7 +7,7 @@ class DatasourceTester(unittest.TestCase):
         self.ds = DataSource()
         user = 'beckerr2'
         password = 'barn787sign'
-        ds.connect(user, password)
+        self.ds.connect(user, password)
 
     def test_in_spotlight_true(self):
         input = "T"
