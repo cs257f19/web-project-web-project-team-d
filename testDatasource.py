582b9eaaa815e80dc447207dd492ba1328ad653a
Lab User <labuser@libr451-04r.its.carleton.edu>
2019-10-31 16:29:54 -0500

    testing

--- testDatasource.py
+++ testDatasource.py
@@ -9,6 +9,7 @@ class DatasourceTester(unittest.TestCase):
         password = 'barn787sign'
         self.ds.connect(user, password)
 
+    #Ensures the 'spotlight' column of each row is true on true input
     def test_in_spotlight_true(self):
         input = "T"
         list = self.ds.getKickstartersInSpotlight(input)
@@ -19,6 +20,7 @@ class DatasourceTester(unittest.TestCase):
                 break
         self.assertTrue(result)
 
+    # Ensures the 'spotlight' column of each row is NOT true on false input
     def test_in_spotlight_true_input_false(self):
         input = "F"
         list = self.ds.getKickstartersInSpotlight(input)
@@ -29,6 +31,7 @@ class DatasourceTester(unittest.TestCase):
                 break
         self.assertFalse(result)
 
+    # Ensures the 'spotlight' column of each row is  false on false input
     def test_in_spotlight_false(self):
         input = "f"
         list = self.ds.getKickstartersInSpotlight(input)
@@ -39,6 +42,7 @@ class DatasourceTester(unittest.TestCase):
                 break
         self.assertTrue(result)
 
+    # Ensures the 'spotlight' column of each row is NOT false on true input
     def test_in_spotlight_false_input_true(self):
         input = "T"
         list = self.ds.getKickstartersInSpotlight(input)
@@ -49,15 +53,13 @@ class DatasourceTester(unittest.TestCase):
                 break
         self.assertFalse(result)
 
+    # Ensures that the returned list is empty
     def test_in_spotlight_bad_input(self):
         input = 0
         list = self.ds.getKickstartersInSpotlight(input)
-        result = True
-        for row in list:
-            if row[10] != input:
-                result = False
-                break
-        self.assertTrue(result)
+
+        self.assertTrue(len(list) == 0)
+
 
 if __name__ == '__main__':
     unittest.main()
