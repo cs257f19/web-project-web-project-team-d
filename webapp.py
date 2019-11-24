4e60019dfb85c739200a060e1c3a3a24d4ce3502
Eric Neidhart <Eric@eduroam-169-202.dyn.carleton.edu>
2019-11-23 19:53:31 -0600

    changes

--- webapp.py
+++ webapp.py
@@ -55,6 +55,8 @@ def defaultComparison():
             newTable = []
             i=0
             for row in table:
+                currency = row[2]
+                pledged = ds.convertCurrency(row[1], currency)
                 rowX = [row[0], row[1]]
                 newTable.append(rowX)
                 i = i+1
