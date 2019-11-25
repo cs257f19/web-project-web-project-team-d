5c79eaa1281e6c7f6f9b3cffb7dfd1d5c5292520
BlueSuperGlue <sonya-romanenko@DESKTOP-PJUE6O2.localdomain>
2019-11-25 09:44:17 -0600

    spotlight and staffpick stuff

--- webapp.py
+++ webapp.py
@@ -75,7 +75,7 @@ def defaultComparison():
 
             newtable_json = json.dumps(newTable)
 
-            return render_template('datapage.html', table=table, field1=field1, field2=field2, newTable = newtable_json)
+            return render_template('datapage.html', table=table, field1=field1, field2=field2, newTable = newtable_json, spotlight=spotlight, staffpick=staffpick)
         elif (field1 == "Backer Count" and field2 == "Goal"):
             table = []
             table = ds.getBackersAndGoal(spot, staff)
@@ -91,7 +91,7 @@ def defaultComparison():
 
             newtable_json = json.dumps(newTable)
 
-            return render_template('datapage.html', table=table, field1=field1, field2=field2, newTable = newtable_json)
+            return render_template('datapage.html', table=table, field1=field1, field2=field2, newTable = newtable_json, spotlight=spotlight, staffpick=staffpick)
         elif (field1 == "Pledged Amount" and field2 == "Goal"):
             table = []
             table = ds.getPledgedAndGoal(spot, staff)
@@ -107,7 +107,7 @@ def defaultComparison():
 
             newtable_json = json.dumps(newTable)
 
-            return render_template('datapage.html', table=table, field1=field1, field2=field2, newTable = newtable_json)
+            return render_template('datapage.html', table=table, field1=field1, field2=field2, newTable = newtable_json, spotlight=spotlight, staffpick=staffpick)
     return render_template('datapage.html')
 
 
@@ -375,8 +375,6 @@ def chartComparison():
 
             else:
                 print ("We fucked up")
-            
-
 
         if (displayVariable == "Staffpick"):
             # all staffpick sorts:
